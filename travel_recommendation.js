96caefc22b81ce5324b2af3f1610e93a8be7977b
buscaroli <m.buscaroli@outlook.com>
2024-03-07 14:31:02 +0000

    work on the creation of the list of destination

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,12 +1,12 @@
 const dataSource = './travel_recommendation_api.json'
 
-function getData(keyword) {
+function getData((keyword, hasSubArray) {
         fetch(dataSource)
         .then(data => data.json())
         .then(destinations => {
-            const list = destinations[keyword].forEach(dest => console.log(dest))
+            generateList(destinations[keyword])
             })  
-        .catch(e => console.log(`Error: ${e}`))
+            .catch(e => console.log(`Error: ${e}`))
 }
 
 function keywordSearch() {
@@ -17,17 +17,17 @@ function keywordSearch() {
     switch(keyword) {
         case("beach"):
         case("beaches"):
-            getData("beaches")
+            getData("beaches", false)
             // clearInput()
             break
         case("country"):
         case("countries"):
-            getData("countries")
+            getData("countries", true)
             // clearInput()
             break
         case("temple"):
         case("temples"):
-            getData("temples")
+            getData("temples", false)
             // clearInput()
             break
         default:
@@ -44,3 +44,53 @@ const searchbarInput = document.getElementById('searchbarInput')
 searchbarInput.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') keywordSearch()
 })
+
+function generateList(list, hasSubArray) {
+    const listElem = document.createElement('div')
+    listElem.className = 'resultList'
+    
+    console.log('- - - - - - -')
+    console.log(list)
+
+    if (list.length === 0) {
+        console.log('nothing found, tell user somehow')
+        return
+    }
+
+    if (hasSubArray) { // country
+
+    } else { // either beach or temple
+        for (item of list) {
+            let newCard = document.createElement('div')
+            newCard.className = 'destinationCard'
+            
+            let newDestName = document.createElement('div')
+            newDestName.className = 'destinationName'
+
+            let newDestImg = document.createElement('img')
+            newDestImg.className = 'destinationImg')
+
+            let newDestDesc = document.createElement('img')
+            newDestDesc.className = 'destinationDesc'
+
+            let newDestBtn = document.createElement('button')
+            newDestBtn.className = 'destinationBtn'
+
+            newCard.append(newDestName)
+            newCard.append(newDestImg)
+            newCard.append(newDestDesc)
+            newCard.append(newDestBtn)
+            listElem.append(newCard)
+
+            listElem.classList.toggle('visibility')
+        }
+    }
+
+}
+
+function destroyList() {
+    const listCreated = document.getElementsByClass('listElem')
+    if (listCreated.length > 0) {
+        listCreated[0].remove()
+    }
+}
